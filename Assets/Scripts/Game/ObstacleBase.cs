bedad88bbdf0af933da3609d4e86d035e00ed51c
Andres Perez Campillo <tomailsandstuff@gmail.com>
2023-04-30 18:52:23 -0500

    Player logic in progress

--- Assets/Scripts/Game/ObstacleBase.cs
+++ Assets/Scripts/Game/ObstacleBase.cs
@@ -10,8 +10,11 @@ public abstract class ObstacleBase : MonoBehaviour
     private new Collider collider;
     private Rigidbody rb;
 
+    [SerializeField]
+    private int hp = 1;
+
     // Start is called before the first frame update
-    private void Start()
+    protected virtual void Start()
     {
         collider = GetComponent<Collider>();
         rb = GetComponent<Rigidbody>();
@@ -19,9 +22,14 @@ public abstract class ObstacleBase : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer.Equals(layerToCollideWith))
+        if (collision.gameObject.layer.Equals(LayerMask.NameToLayer(GameConstants.BULLET_HARD_LAYER_NAME)))
         {
-            //Event to update score
+        }
+        else if (collision.gameObject.layer.Equals(LayerMask.NameToLayer(GameConstants.BULLET_MID_LAYER_NAME)))
+        {
+        }
+        else
+        {
         }
 
         Destroy(gameObject);
--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -4,17 +4,25 @@ public class PlayerController : MonoBehaviour
 {
     private Player player = new Player();
 
+    [Header("Movement params")]
+    [SerializeField]
+    [Range(20F, 50F)]
+    private float speed = 20F;
+
+    [Header("Shoot params")]
     [SerializeField]
     private Rigidbody[] bulletPrefabs;
-
-    private Rigidbody selectedBullet;
 
     [SerializeField]
     private Transform spawnPos;
 
     [SerializeField]
-    private static float shootForce;
+    [Range(0F, 500F)]
+    private float shootForce = 250F;
 
+    private Rigidbody selectedBullet;
+
+    private float hVal = 0F;
     public string PlayerScore { get => player?.Score.ToString(); }
 
     // Start is called before the first frame update
@@ -22,15 +30,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        ProcessInput();
+        hVal = Input.GetAxis("Horizontal");
+        transform.Translate(transform.right * hVal * speed * Time.deltaTime);
+
+        ProcessShooting();
     }
 
-    private void ProcessInput()
+    private void ProcessShooting()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Fire
-            if (selectedBullet != null && spawnPos != null)
+            if (selectedBullet == null)
+            {
+                SelectBullet();
+            }
+
+            if (spawnPos != null)
             {
                 Instantiate(selectedBullet, spawnPos.position, spawnPos.rotation).AddForce(transform.forward * shootForce, ForceMode.Force);
             }
@@ -39,19 +55,24 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             //Switch to bullet 1
-            selectedBullet = bulletPrefabs[0];
+            SelectBullet();
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             //Switch to bullet 2
-            selectedBullet = bulletPrefabs[1];
+            SelectBullet(1);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             //Switch to bullet 3
-            selectedBullet = bulletPrefabs[2];
+            SelectBullet(2);
         }
+    }
+
+    private void SelectBullet(int index = 0)
+    {
+        selectedBullet = bulletPrefabs[Mathf.Clamp(index, 0, 2)];
     }
 }
